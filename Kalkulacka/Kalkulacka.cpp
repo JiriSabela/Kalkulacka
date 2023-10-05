2d9624594ea10c4f6e41dd15b0f173d04baf5761
Student <Student@PC-SE5113-19>
2023-10-05 12:12:04 +0200

    Add project files.

---
+++ Kalkulacka/Kalkulacka.cpp
@@ -0,0 +1,51 @@
+﻿// Kalkulacka.cpp : Defines the entry point for the application.
+//
+
+#include "Kalkulacka.h"
+
+int main(void)
+{
+	int volba, zaokrouhl;
+	double cislo, vysledek;
+	
+	printf("\nZadejte cislo:");
+	scanf("%lf", &cislo);
+
+	do
+	{
+
+		printf("\n1. Druha mocnina\n");
+		printf("2. Druha odmocnina\n");
+		printf("3. Zaokrouhlena hodnota\n");
+		printf("4. Faktorial ze zaokrouhlene hodnoty\n");
+		printf("Volba:");
+		scanf("%d", &volba);
+		switch (volba)
+		{
+		case 1:
+			vysledek = pow(cislo, 2);
+			printf("%lf", vysledek);
+			break;
+		case 2:
+			vysledek = sqrt(cislo);
+			printf("%lf",vysledek);
+			break;
+		case 3:
+			zaokrouhl = round(cislo);
+			printf("%d", zaokrouhl);
+			break;
+		case 4:
+			vysledek = 1;
+			for (int i=1; i<= zaokrouhl; i++)
+			{
+				vysledek = vysledek * i;
+			}
+			printf("Faktorial: %.lf",vysledek);
+			break;
+		default:
+			break;
+		}
+	} while (volba != 0);
+
+	return 0;
+}
---
+++ Kalkulacka/Kalkulacka.h
@@ -0,0 +1,10 @@
+﻿// Kalkulacka.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// TODO: Reference additional headers your program requires here.
